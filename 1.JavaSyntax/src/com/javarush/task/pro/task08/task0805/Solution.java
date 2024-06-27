8e495d1fe640e3edefe234b6738bc05e3444a0c3
nikaminaeva719 <meraylatin@yandex.ru>
2024-06-28 00:49:45 +0300

    Add solution JavaSyntax task 0504

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0805/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task08.task0805;
+
+/* 
+Magic 8 ball
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Сдам ли я экзамены?");
+        System.out.println(MagicBall.getPrediction());
+    }
+
+}
