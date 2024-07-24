471193f6fe435e4abf1088c17a8b05ba7eeb0158
nikaminaeva719 <meraylatin@yandex.ru>
2024-07-25 00:24:52 +0300

    Add solution JavaSyntax task 0913

--- 1.JavaSyntax/src/com/javarush/task/pro/task09/task0913/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0913/Solution.java
@@ -15,12 +15,12 @@ public class Solution {
     }
 
     public static int getIndexFromFirstWord(String string, String word) {
-        //напишите тут ваш код
-        return 0;
+
+        return string.indexOf(word);
     }
 
     public static int getIndexFromLastWord(String string, String word) {
         //напишите тут ваш код
-        return 0;
+        return string.lastIndexOf(word);
     }
 }
