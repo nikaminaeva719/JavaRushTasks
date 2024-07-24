b973f69a2e586e3256d816bd6bed8d39120a8767
nikaminaeva719 <meraylatin@yandex.ru>
2024-07-25 00:26:23 +0300

    Add solution JavaSyntax task 1006

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task10.task1006;
+
+/* 
+Кто тут наследник?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        ElectricCar elc = new ElectricCar();
+        HybridCar hbr = new HybridCar();
+        GasCar sc = new GasCar();
+    }
+}
