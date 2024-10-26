eb0ad1b80d5e9845f9c33262c142a70fcb15fcb3
nikaminaeva719 <meraylatin@yandex.ru>
2024-10-26 22:27:37 +0300

    Add solution JavaCore 1206, 1207, 1208, 1209, 1210

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(String a) {
+
+    }
+
+    public static void print(int a){
+
+    }
+
+    public static void print(double a){
+
+    }
+
+    public static void print(String b, int a){
+
+    }
+
+    public static void print(double a, int b){
+
+    }
+}
