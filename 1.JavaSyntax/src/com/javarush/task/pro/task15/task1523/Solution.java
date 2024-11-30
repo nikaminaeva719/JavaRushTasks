19731ab6682d4ea0030833636b2aa1d3afc44744
nikaminaeva719 <meraylatin@yandex.ru>
2024-11-30 23:52:14 +0300

    Add solution JavaSyntax 1522, 1523

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1523/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task15.task1523;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+/* 
+Получение информации по API
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
+
