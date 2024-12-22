360a119a74bb00d981304e330ff315b9586cc34a
nikaminaeva719 <meraylatin@yandex.ru>
2024-12-22 23:27:42 +0300

    Add solution JavaCore tasks 1505, 1506, 1507

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1505/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task15.task1505;
+
+/* 
+ООП - исправь ошибки в наследовании
+*/
+
+public class Solution {
+    public interface HasWeight {
+        int getWeight();
+    }
+
+    public interface HasHeight {
+        int getHeight();
+    }
+
+    public static class Human implements HasWeight, HasHeight {
+
+        public int getHeight() {
+            return 160;
+        }
+
+        public int getWeight() {
+            return 55;
+        }
+    }
+
+    public static void main(String[] args) {
+        Human human = new Human();
+        System.out.println(human.getHeight());
+        System.out.println(human.getWeight());
+    }
+}
