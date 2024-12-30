7336827b8d2ee4081a5eab928f957a0e91c951af
nikaminaeva719 <meraylatin@yandex.ru>
2024-12-31 00:05:16 +0300

    Add solution JavaCore tasks 1523, 1524

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+
+    public Solution(){}
+
+    protected Solution(int value){}
+
+    Solution (double value){}
+
+    private Solution(float value){}
+
+    public static void main(String[] args) {
+
+    }
+}
+
