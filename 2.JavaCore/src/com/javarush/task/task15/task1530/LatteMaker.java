7c8d31c9dcb52037ca1205a9d5329ce563c57980
nikaminaeva719 <meraylatin@yandex.ru>
2025-01-06 22:59:44 +0300

    Add solution JavaCore tasks 1530, 1531

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1530/LatteMaker.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task15.task1530;
+
+public class LatteMaker extends DrinkMaker {
+
+    void getRightCup() {
+        System.out.println("Берем чашку для латте");
+    }
+
+    void putIngredient() {
+        System.out.println("Делаем молоко с пенкой");
+    }
+
+    void pour() {
+        System.out.println("Заливаем кофе");
+    }
+
+}
