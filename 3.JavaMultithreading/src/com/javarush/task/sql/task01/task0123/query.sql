839b3fc2787d568eeb2f6e323266a630240038fb
nikaminaeva719 <meraylatin@yandex.ru>
2025-04-02 23:43:39 +0300

    Add solution SQL & Hibernate tasks 0123, 0124

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0123/query.sql
@@ -0,0 +1,4 @@
+-- Write your code here:
+SELECT *
+FROM car
+WHERE brand IN ('renault', 'opel', 'seat', 'skoda');
---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0124/query.sql
@@ -0,0 +1,4 @@
+-- Write your code here:
+SELECT brand, model, booked_quantity
+from car
+where model like 'k%';
