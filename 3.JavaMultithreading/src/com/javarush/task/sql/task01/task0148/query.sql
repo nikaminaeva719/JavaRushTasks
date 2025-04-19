d02bdb689edde869e0deb7e956541a82a84d60c5
nikaminaeva719 <meraylatin@yandex.ru>
2025-04-19 23:52:11 +0300

    Add solution SQL & Hibernate tasks 0148-0150

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0148/query.sql
@@ -0,0 +1,4 @@
+-- Write your code here:
+SELECT required, identifier, description
+FROM parts
+WHERE description IS NOT NULL;
---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task01/task0149/query.sql
@@ -0,0 +1,5 @@
+-- Write your code here:
+SELECT *
+FROM parts
+WHERE description IS NOT NULL
+   OR identifier IS NULL;
