590fa64b181941a5f04c5f9e9647afb3021c75bd
nikaminaeva719 <meraylatin@yandex.ru>
2025-04-30 23:27:51 +0300

    Add solution SQL & Hibernate tasks 0421-0423

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0421/query.sql
@@ -0,0 +1,2 @@
+-- Write your code here:
+select curdate(), adddate(curdate(), 3), subdate(curdate(), 3);
---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0422/query.sql
@@ -0,0 +1,2 @@
+-- Write your code here:
+select now(), adddate(now(), interval null second), adddate(now(), interval 33 second);
