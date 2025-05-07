46b4507e4285e8bfc7465222c9ef1c6f338380c1
nikaminaeva719 <meraylatin@yandex.ru>
2025-05-07 23:41:20 +0300

    Add solution SQL & Hibernate tasks 0440, 0441

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0440/query.sql
@@ -0,0 +1,4 @@
+-- Write your code here:
+select name,
+       to_base64(name)
+from employee;
---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0441/query.sql
@@ -0,0 +1,4 @@
+-- Write your code here:
+select name_b64,
+       from_base64(name_b64)
+from employee;
