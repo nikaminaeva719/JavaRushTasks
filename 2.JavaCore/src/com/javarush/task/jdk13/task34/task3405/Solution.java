112692bcc3785485d24dd679dd0ed16c3b16a040
nikaminaeva719 <meraylatin@yandex.ru>
2025-06-24 21:58:11 +0300

    Add solution Core task 3405 part 2

--- 2.JavaCore/src/com/javarush/task/jdk13/task34/task3405/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3405/Solution.java
@@ -1,9 +1,13 @@
 package com.javarush.task.jdk13.task34.task3405;
 
+import lombok.SneakyThrows;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /* 
 Нарушение приватности
@@ -19,7 +23,7 @@ public class Solution {
         print(getFields(task));
     }
 
-    public static Map<String, Object> getFields(Object object) throws Exception {
+    public static Map<String, Object> getFields1(Object object) throws Exception {
         Map<String, Object> map = new HashMap<>();
         Class clazz = object.getClass();
         Field[] fields = clazz.getDeclaredFields();
@@ -30,6 +34,18 @@ public class Solution {
             }
         }
         return map;
+    }
+
+    @SneakyThrows
+    private static Object fieldGetObject(Field field, Object object) {
+        return field.get(object);
+    }
+
+    public static Map<String, Object> getFields(Object object) throws Exception {
+        return Arrays.stream(object.getClass().getDeclaredFields())
+                .filter(field -> Modifier.isPrivate(field.getModifiers()))
+                .peek(field -> field.setAccessible(true))
+                .collect(Collectors.toMap(field -> field.getName(), field -> fieldGetObject(field, object)));
     }
 
     public static void print(Map<?, ?> fields) {
