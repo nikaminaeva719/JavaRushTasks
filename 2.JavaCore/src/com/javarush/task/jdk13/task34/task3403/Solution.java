5a87526a5c52ef92f47c4a5611a3d50debeee647
nikaminaeva719 <meraylatin@yandex.ru>
2025-06-27 22:37:30 +0300

    Add solution Core task 3403 — 3406

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3403/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.jdk13.task34.task3403;
+
+/* 
+Разложение на множители с помощью рекурсии
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public void recurse(int n) {
+
+    }
+}
