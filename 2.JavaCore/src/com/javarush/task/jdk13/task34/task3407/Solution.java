b3d64d448fc324f54d87dbc90902c084d1a8420f
nikaminaeva719 <meraylatin@yandex.ru>
2025-06-28 23:49:12 +0300

    Add solution Core task 3407 — 3409

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3407/Solution.java
@@ -0,0 +1,49 @@
+package com.javarush.task.jdk13.task34.task3407;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.function.BiFunction;
+
+/* 
+Собиратель типов
+*/
+
+public class Solution {
+
+    private Map<TimeUnit, BiFunction<Integer, Long, Map<Boolean, List<String>>>> field;
+
+    public static void main(String[] args) throws Exception {
+        getTypes(Solution.class.getDeclaredField("field").getGenericType()).forEach(System.out::println);
+    }
+
+    public static Set<Type> getTypes1(Type type) {
+        Set<Type> set = new LinkedHashSet<>();
+        if (type instanceof ParameterizedType paramType) {
+            Type rawType = paramType.getRawType();
+            set.add(rawType);
+            Type[] actualTypeArguments = paramType.getActualTypeArguments();
+            for (Type actualTypeArgument : actualTypeArguments) {
+                Set<Type> types = getTypes1(actualTypeArgument);
+                set.addAll(types);
+            }
+        } else {
+            set.add(type);
+        }
+        return set;
+    }
+
+    public static Set<Type> getTypes(Type type) {
+        Set<Type> set = new LinkedHashSet<>();
+        if (type instanceof ParameterizedType paramType) {
+            set.add(paramType.getRawType());
+            Arrays.stream(paramType.getActualTypeArguments())
+                    .map(Solution::getTypes)
+                    .forEach(set::addAll);
+        } else {
+            set.add(type);
+        }
+        return set;
+    }
+}
