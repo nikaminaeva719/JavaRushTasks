4af19bfad7112c1ba6777ee1e40e2fc703f099e9
nikaminaeva719 <meraylatin@yandex.ru>
2025-09-24 23:42:25 +0300

    Add solution Multithreading task 4206

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task42/task4206/Storage.java
@@ -0,0 +1,26 @@
+package com.javarush.task.jdk13.task42.task4206;
+
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class Storage {
+    private final BlockingDeque<Integer> deque = new LinkedBlockingDeque<>(2);
+
+    public void take() {
+        try {
+            Integer value = deque.take();
+            System.out.println("take - " + value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void put(int i) {
+        try {
+            deque.put(i);
+            System.out.println("put - " + i);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
