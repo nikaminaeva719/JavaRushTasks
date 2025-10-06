7d2ac4add4f93e01a3ebf9ea0e73696f5bb75575
nikaminaeva719 <meraylatin@yandex.ru>
2025-10-06 23:42:45 +0300

    Add solution Multithreading task 4307

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task43/task4307/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.jdk13.task43.task4307;
+
+//import org.apache.commons.lang3.ObjectUtils;
+
+/* 
+Шило на мыло
+*/
+
+import org.apache.commons.lang3.ObjectUtils;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String[] array1 = {null, "hello", "ok", null, "bye"};
+
+        printFirstNonNull(array1);
+        System.out.println("----------");
+        printDefaultValueIfNullObject(array1, "oops");
+    }
+
+    public static void printFirstNonNull(final String... values) {
+        if (Objects.isNull(values)) {
+            System.out.println((Object) null);
+            return;
+        }
+
+        Arrays.stream(values)
+                .filter(Objects::nonNull)
+                .findFirst()
+                .ifPresent(System.out::println);
+    }
+
+    public static void printDefaultValueIfNullObject(final String[] values, final String defaultValue) {
+
+        Arrays.stream(values)
+                .map(str -> Objects.requireNonNullElse(str, defaultValue))
+                .forEach(System.out::println);
+    }
+}
