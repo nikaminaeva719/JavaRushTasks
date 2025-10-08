153e74b94057ae9dda69c09fa08ef08f7eb0b07e
nikaminaeva719 <meraylatin@yandex.ru>
2025-10-08 21:54:01 +0300

    Add solution Multithreading task 3110 part 6

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,17 +1,23 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.command.ExitCommand;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
 
 public class Archiver {
     public static void main(String[] args) throws Exception {
-        System.out.println("Введите полный путь, куда сохранить архив: ");
+        ConsoleHelper.writeMessage("Введите полный путь, куда сохранить архив: ");
         BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
         ZipFileManager zipFileManager = new ZipFileManager(Paths.get(console.readLine()));
-        System.out.println("Введите путь до папки файла, который нужно сохранить в архив: ");
+        ConsoleHelper.writeMessage("Введите путь до папки файла, который нужно сохранить в архив: ");
         zipFileManager.createZip(Paths.get(console.readLine()));
 
+        ExitCommand exitCommand =  new ExitCommand();
+        exitCommand.execute();
+
     }
+
+
 }
