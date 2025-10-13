e571db846551bd620ed59be7c7c502db8621729a
nikaminaeva719 <meraylatin@yandex.ru>
2025-10-13 22:54:56 +0300

    Add solution SQL & Hibernate tasks 1003, 1004

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task10/task1003/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.sql.task10.task1003;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+/* 
+task1003
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        promoteAll();
+    }
+
+    public static void promoteAll() {
+        try (Session session = MySessionFactory.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            String hql = "update Employee  set smth = concat('senior',smth)";
+            Query query = session.createQuery(hql);
+            query.executeUpdate();
+            transaction.commit();
+
+        }
+
+    }
+}
