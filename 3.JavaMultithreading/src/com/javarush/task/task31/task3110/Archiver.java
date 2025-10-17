aaadccfdde472565f7eb85c9411b193d0a646750
nikaminaeva719 <meraylatin@yandex.ru>
2025-10-17 17:25:56 +0300

    Add solution Multithreading task 3110 part 8

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,22 +1,41 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
 
 public class Archiver {
     public static void main(String[] args) throws Exception {
-        ConsoleHelper.writeMessage("Введите полный путь, куда сохранить архив: ");
-        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(console.readLine()));
-        ConsoleHelper.writeMessage("Введите путь до папки файла, который нужно сохранить в архив: ");
-        zipFileManager.createZip(Paths.get(console.readLine()));
+        Operation operation = null;
+        while (operation != Operation.EXIT){
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e){
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
 
-        ExitCommand exitCommand =  new ExitCommand();
-        exitCommand.execute();
+        }
 
+    }
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:\n" +
+                "0 - упаковать файлы в архив\n" +
+                "1 - добавить файл в архив\n" +
+                "2 - удалить файл из архива\n" +
+                "3 - распаковать архив\n" +
+                "4 - просмотреть содержимое архива\n" +
+                "5 - выход");
+        int operationNumber = ConsoleHelper.readInt();
+        Operation[] operations = Operation.values();
+        return operations[operationNumber];
     }
 
 
