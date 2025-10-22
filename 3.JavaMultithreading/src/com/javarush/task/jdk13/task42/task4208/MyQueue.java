de89f81ea9d0e87b6eca812009631f8d842ae344
nikaminaeva719 <meraylatin@yandex.ru>
2025-10-22 23:40:10 +0300

    Add solution Multithreading task 4208

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task42/task4208/MyQueue.java
@@ -0,0 +1,69 @@
+package com.javarush.task.jdk13.task42.task4208;
+
+import java.util.AbstractQueue;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/* 
+Потоки в очередь!
+*/
+
+public class MyQueue extends AbstractQueue<String> {
+
+    private final List<String> values = new ArrayList<>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Lock writeLock = lock.writeLock();
+    private final Lock readLock = lock.readLock();
+
+
+
+    @Override
+    public Iterator<String> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int size() {
+        try {
+            readLock.lock();
+            return values.size();
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    @Override
+    public boolean offer(String o) {
+        try {
+            writeLock.lock();
+            values.add(o);
+            return true;
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    @Override
+    public String poll() {
+        try {
+            writeLock.lock();
+            return values.size() > 0 ? values.remove(0) : null;
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    @Override
+    public String peek() {
+        try {
+            readLock.lock();
+            return values.size() > 0 ? values.get(0) : null;
+        } finally {
+            readLock.unlock();
+        }
+    }
+}
